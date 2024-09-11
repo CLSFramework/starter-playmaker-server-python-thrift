833e120aa159644b6efb392333d39cd2dcf489ef
ErfanFathi <fathye897@gmail.com>
2024-09-11 11:47:45 -0700

    pass completion

--- src/DM_WithBall.py
+++ src/DM_WithBall.py
@@ -7,6 +7,8 @@ import time
 import logging
 from soccer.ttypes import *
 from src.Shoot import Shoot
+from src.Pass import Pass
+from src.Dribble import Dribble
 
 class WithBallDecisionMaker(IDecisionMaker):
     def __init__(self):
@@ -15,13 +17,15 @@ class WithBallDecisionMaker(IDecisionMaker):
     sum_time = 0
     count = 0
     def make_decision(self, agent: IAgent):
-
+ 
         agent.add_log_message(LoggerLevel.TEAM ,
                                     f": DM_WithBall" ,
                                         agent.wm.myself.position.x ,
                                             agent.wm.myself.position.y - 2 ,
                                                 '\033[31m')
-        #Shoot.decision(self, agent)
+        #Shoot().decision(self, agent)
+        #Pass().Decision(agent)
+        #Dribble().Decision(agent)
         agent.add_action(PlayerAction(helios_chain_action=HeliosChainAction(lead_pass=True,
                                                                                   direct_pass=True,
                                                                                   through_pass=True,
--- src/Pass.py
+++ src/Pass.py
@@ -14,4 +14,39 @@ class Pass:
 
     def Decision(agent: IAgent):
         Targets = agent.wm.teammates
-        
+        BallPosition = agent.wm.ball.position
+        AgentPosition = agent.wm.myself.position
+        for i in Targets :
+            if i == None or i.uniform_number == agent.wm.myself.uniform_number or i.uniform_number < 0.0:
+                continue
+            if i.position.dist(BallPosition) > 30.0 :
+                continue
+            if AgentPosition.dist(i.position) < 2.0:
+                continue
+            Possibility = Sector2D(BallPosition,
+                                    1.0,
+                                        i.position.dist(BallPosition) + 3.0,
+                                            Vector2D(i.position - BallPosition).th() - 15.0,
+                                                Vector2D(i.position - BallPosition).th() + 15.0)
+            if ExistOpponentIn(agent, Possibility):
+                Targets.remove(i)
+        if not Targets == []:
+            BestTarget = Targets[0]
+            for i in Targets:
+                if i.position.x > BestTarget.position.x :
+                    BestTarget = i.position
+            if not agent.wm.game_mode_type == GameModeType(2):
+                agent.add_action(PlayerAction(body_smart_kick=Body_SmartKick(BestTarget,
+                    2.7,
+                        2.5,
+                            1)))
+            else :
+                agent.add_action(PlayerAction(body_smart_kick=Body_SmartKick(BestTarget,
+                    2.7,
+                        2.5,
+                            1)))  
+                
+def ExistOpponentIn(agent: IAgent, region: Region2D):
+        for i in agent.wm.opponents:
+            if region.contains(i.position):
+               return True 
