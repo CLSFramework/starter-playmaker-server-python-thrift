6f73177c0a28cb4649d0b226edaf0b2e0d3183d0
ErfanFathi <fathye897@gmail.com>
2024-09-16 23:51:28 -0700

    feat: added executeV12 to Tackle

--- src/BhvBasicTackle.py
+++ src/BhvBasicTackle.py
@@ -1,6 +1,8 @@
 from src.IAgent import IAgent
-from soccer.ttypes import PlayerAction, CardType, Tackle
+from soccer.ttypes import PlayerAction, CardType, Tackle, LoggerLevel,Neck_TurnToBallOrScan
+from src.Tools import Tools
 from pyrusgeom.vector_2d import Vector2D
+from pyrusgeom.angle_deg import AngleDeg
 from pyrusgeom.soccer_math import inertia_n_step_point
 from pyrusgeom.ray_2d import Ray2D
 from pyrusgeom.line_2d import Line2D
@@ -69,7 +71,108 @@ class BhvBasicTackle:
         
         return
         
+    def executeV12(self, agent: IAgent):
 
+        actions = []
+        s_last_execute_time =  agent.wm.cycle
+        s_result = False
+        s_best_angle = AngleDeg(0,0)
+
+        wm = agent.wm
+
+        if s_last_execute_time == wm.time():
+            agent.add_log_text(LoggerLevel.TEAM, f": called several times")
+            if s_result:
+                agent.add_log_text(LoggerLevel.TEAM, f"{__file__}: executeV12() tackle angle={s_best_angle.degree()}")
+                agent.add_log_message(LoggerLevel.TEAM,f"BasicTackle{s_best_angle.degree()}", agent.wm.myself.position.x, agent.wm.myself.position.y - 2, '\033[31m')
+
+                tackle_dir = (s_best_angle - wm.myself.body_direction).degree()
+                actions.append(PlayerAction(tackle=Tackle(tackle_dir)))
+                actions.append(PlayerAction(neck_turn_to_ball_or_scan=Neck_TurnToBallOrScan(0)))
+            return actions
+
+        s_last_execute_time = wm.time()
+        s_result = False
+
+        SP = agent.serverParams
+
+        opp_goal = Vector2D(SP.pitch_half_length, 0.0)
+        our_goal = Vector2D(-SP.pitch_half_length, 0.0)
+        virtual_accel = (wm.kickable_opponent_id and Vector2D(our_goal - wm.ball.position).set_length(0.5) or Vector2D(0.0, 0.0))
+        shoot_chance = (wm.ball.position.dist(opp_goal) < 20.0)
+
+        ball_rel_angle = wm.ball.angleFromSelf() - wm.myself.body_direction
+        tackle_rate = SP.tackle_power_rate * (1.0 - 0.5 * abs(ball_rel_angle) / 180.0)
+
+        best_angle = AngleDeg(0.0)
+        max_speed = -1.0
+
+        for a in range(-180.0, 180.0, 10.0):
+            rel_angle = AngleDeg(a - wm.myself.body_direction)
+
+            eff_power = SP.max_tackle_power + ((SP.max_tackle_power - SP.max_back_tackle_power) * (1.0 - rel_angle.abs() / 180.0))
+            eff_power *= tackle_rate
+
+            vel = Vector2D(wm.ball.velocity + Vector2D.polar2vector(eff_power, AngleDeg(a)))
+            vel += virtual_accel
+
+            speed = vel.r()
+            if speed > SP.ball_speed_max:
+                vel *= (SP.ball_speed_max / speed)
+                speed = SP.ball_speed_max
+
+            if abs(vel.th()) > 90.0:
+                continue
+
+            ball_next = wm.ball.position + vel
+
+            maybe_opponent_get_ball = False
+
+            for o in wm.opponents:
+                if o.pos_count > 10:
+                    continue
+                if o.ghost_count > 0:
+                    continue
+                if o.is_tackling:
+                    continue
+                if o.dist_from_ball > 6.0:
+                    break
+
+                opp_pos = Vector2D(o.position + o.velocity)
+                if opp_pos.dist(ball_next) < SP.kickable_area + 0.1:
+                    maybe_opponent_get_ball = True
+                    break
+
+            if maybe_opponent_get_ball:
+                continue
+
+            if shoot_chance:
+                ball_ray = Ray2D(wm.ball.position, vel.th())
+                goal_line = Line2D(Vector2D(SP.pitch_half_length, 10.0), Vector2D(SP.pitch_half_length, -10.0))
+                intersect = Vector2D(ball_ray.intersection(goal_line))
+                if intersect._is_valid and intersect.abs_y() < (SP.goal_width/2.0) - 3.0:
+                    speed += 10.0
+
+            if speed > max_speed:
+                max_speed = speed
+                best_angle = AngleDeg(a)
+
+        if max_speed < 0.0:
+            s_result = False
+            agent.add_log_text(LoggerLevel.TEAM, f"{__file__}: failed executeV12. max_speed={max_speed}")
+            return False
+
+        s_result = True
+        s_best_angle = best_angle
+
+        agent.add_log_text(LoggerLevel.TEAM, f"{__file__}: executeV12() angle={best_angle.degree()}")
+        agent.add_log_message(LoggerLevel.TEAM,f"BasicTackle{best_angle.degree()}", agent.wm.myself.position.x, agent.wm.myself.position.y - 2, '\033[31m')
+
+        tackle_dir = (best_angle - wm.myself.body_direction).degree()
+        actions.append(PlayerAction(tackle=Tackle(tackle_dir)))
+        actions.append(PlayerAction(neck_turn_to_ball_or_scan=Neck_TurnToBallOrScan(0)))
+
+        return actions
         
         
         
